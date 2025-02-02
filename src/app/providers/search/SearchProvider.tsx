1818a6937adbd584f32385d74d782136ef1305a7
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-02 22:26:36 +0500

    fix: fix ts errors using search provider by declaring type of context type explicitly

--- src/app/providers/search/SearchProvider.tsx
+++ src/app/providers/search/SearchProvider.tsx
@@ -1,12 +1,22 @@
 import { createContext, Component, type ReactNode } from 'react';
 
-const INITIAL_VALUE = 'search context';
-
-export const SearchContext = createContext(INITIAL_VALUE);
+interface SearchContextValue {
+  search: string;
+  setSearch: (value: string) => void;
+}
 
 interface SearchProviderProps {
   children?: ReactNode;
 }
+
+const INITIAL_VALUE = {
+  search: '123',
+  setSearch: (value: string) => {
+    console.log(value);
+  },
+};
+
+export const SearchContext = createContext<SearchContextValue>(INITIAL_VALUE);
 
 export class SearchProvider extends Component<SearchProviderProps> {
   render() {
--- src/features/search-form/ui/SearchForm.tsx
+++ src/features/search-form/ui/SearchForm.tsx
@@ -2,12 +2,13 @@ import { Component, createRef } from 'react';
 import { SearchContext } from '@/app/providers/search';
 
 export class SearchForm extends Component {
+  declare context: React.ContextType<typeof SearchContext>;
   static contextType = SearchContext;
 
   ref = createRef<HTMLInputElement>();
 
   render() {
-    console.log(this.context);
+    this.context.setSearch('3213123');
 
     return (
       <form
