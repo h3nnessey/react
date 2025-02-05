bfe873d356ce668790be887890ac446e8252e594
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-06 00:12:15 +0500

    refactor: replace CC with FC for SearchForm

--- src/features/search-form/ui/SearchForm.tsx
+++ src/features/search-form/ui/SearchForm.tsx
@@ -1,4 +1,4 @@
-import { createRef, Component, type FormEvent } from 'react';
+import { useRef, type FormEvent } from 'react';
 import { Input, Button } from '@/shared/ui/';
 import styles from './SearchForm.module.scss';
 
@@ -8,32 +8,32 @@ interface SearchFormProps {
   onSubmit: (query: string) => void;
 }
 
-export class SearchForm extends Component<SearchFormProps> {
-  inputRef = createRef<HTMLInputElement>();
+export const SearchForm = ({
+  onSubmit,
+  defaultValue,
+  disabled,
+}: SearchFormProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const query = this.inputRef.current?.value.trim() || '';
+    const query = inputRef.current?.value.trim() || '';
 
-    this.props.onSubmit(query);
+    onSubmit(query);
   };
 
-  render() {
-    const { disabled, defaultValue } = this.props;
-
-    return (
-      <form className={styles.form} onSubmit={this.handleSubmit}>
-        <Input
-          inputRef={this.inputRef}
-          disabled={disabled}
-          defaultValue={defaultValue}
-          placeholder="Search something..."
-        />
-        <Button type="submit" disabled={disabled}>
-          Search
-        </Button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <Input
+        inputRef={inputRef}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        placeholder="Search something..."
+      />
+      <Button type="submit" disabled={disabled}>
+        Search
+      </Button>
+    </form>
+  );
+};
