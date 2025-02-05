43dfb9fd9b9cc311a8eab550d43bab0f7ac9ace3
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-06 00:07:38 +0500

    refactor: replace CC with FC for Loader

--- src/shared/ui/loader/ui/Loader.tsx
+++ src/shared/ui/loader/ui/Loader.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { classnames } from '@/shared/lib/styling';
 import styles from './Loader.module.scss';
 
@@ -6,12 +5,10 @@ interface LoaderProps {
   className?: string;
 }
 
-export class Loader extends Component<LoaderProps> {
-  render() {
-    return (
-      <div className={classnames(styles.container, this.props.className)}>
-        <span className={styles.loader}></span>
-      </div>
-    );
-  }
-}
+export const Loader = ({ className }: LoaderProps) => {
+  return (
+    <div className={classnames(styles.container, className)}>
+      <span className={styles.loader}></span>
+    </div>
+  );
+};
