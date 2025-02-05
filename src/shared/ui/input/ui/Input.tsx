f13b2d9faf9f3420b3ba9a2455e55cc93f13d786
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-06 00:06:43 +0500

    refactor: replace CC with FC for Input

--- src/shared/ui/input/ui/Input.tsx
+++ src/shared/ui/input/ui/Input.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import type { InputHTMLAttributes, RefObject } from 'react';
 import { classnames } from '@/shared/lib/styling';
 import styles from './Input.module.scss';
@@ -8,16 +7,12 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
 };
 
-export class Input extends Component<InputProps> {
-  render() {
-    const { inputRef, className, ...restProps } = this.props;
-
-    return (
-      <input
-        className={classnames(styles.input, className)}
-        ref={inputRef}
-        {...restProps}
-      />
-    );
-  }
-}
+export const Input = ({ inputRef, className, ...props }: InputProps) => {
+  return (
+    <input
+      className={classnames(styles.input, className)}
+      ref={inputRef}
+      {...props}
+    />
+  );
+};
