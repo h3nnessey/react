996c7f44ae7d99a5dbc05ff14991c7aadbe954f0
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-06 00:01:55 +0500

    refactor: replace CC with FC for Card

--- src/shared/ui/card/ui/Card.tsx
+++ src/shared/ui/card/ui/Card.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { classnames } from '@/shared/lib/styling';
 import styles from './Card.module.scss';
 
@@ -9,18 +8,19 @@ export interface CardProps {
   className?: string;
 }
 
-export class Card extends Component<CardProps> {
-  render() {
-    const { imageUrl, title, description, className } = this.props;
-
-    return (
-      <div className={classnames(styles.card, className)}>
-        <img className={styles.image} src={imageUrl} alt={title} />
-        <div className={styles.about}>
-          <p className={styles.title}>{title}</p>
-          <p className={styles.description}>{description}</p>
-        </div>
+export const Card = ({
+  imageUrl,
+  title,
+  description,
+  className,
+}: CardProps) => {
+  return (
+    <div className={classnames(styles.card, className)}>
+      <img className={styles.image} src={imageUrl} alt={title} />
+      <div className={styles.about}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
