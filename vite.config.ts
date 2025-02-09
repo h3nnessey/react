14a583483701d59b99b9f7bbfacd27e48fd9d96b
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-09 22:40:43 +0500

    chore: add testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: './',
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
     },
   },
 });
