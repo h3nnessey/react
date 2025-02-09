676042d5fe4e54285c985fc2d280d3cf16a5e6e0
Vitaly <vitaliy.zayakin@gmail.com>
2025-02-09 22:05:31 +0500

    feat: remove error button

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
 import { SearchForm } from '@/features/search-form';
-import { Button } from '@/shared/ui';
 import styles from './Header.module.scss';
 
 export const Header = () => {
-  const [hasError, setHasError] = useState(false);
-
-  const handleThrowErrorClick = () => {
-    setHasError(true);
-  };
-
-  if (hasError) {
-    throw new Error('Test Error');
-  }
-
   return (
     <header className={styles.header}>
-      <Button onClick={handleThrowErrorClick} variant="danger">
-        Error
-      </Button>
       <SearchForm />
     </header>
   );
