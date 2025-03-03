8beecaf6735b0a7065aac6dfd65128d15f1ce94e
Vitaly <vitaliy.zayakin@gmail.com>
2025-03-03 12:35:37 +0500

    feat: remove layout from 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,20 @@
 import Link from 'next/link';
 import { ErrorMessage } from '@/shared/ui/components';
-import Layout from './layout';
+import { classnames } from '@/shared/lib/styling';
+import { Theme } from '@/shared/ui/theme';
 import styles from '@/styles/not-found-page/NotFoundPage.module.scss';
 
 export default function NotFoundPage() {
   return (
-    <Layout>
-      <div>
-        <ErrorMessage message="404 | Not Found">
-          <div className={styles.error}>
-            <p>The page you are looking for does not exist</p>
-            <p>
-              Do a search or
-              <Link className={styles.link} href="/" replace>
-                Go Home
-              </Link>
-            </p>
-          </div>
-        </ErrorMessage>
-      </div>
-    </Layout>
+    <div className={classnames(styles.container, Theme.Dark)}>
+      <ErrorMessage message="404 | Not Found">
+        <div className={styles.error}>
+          <p>The page you are looking for does not exist</p>
+          <Link className={styles.link} href="/" replace>
+            Go Home
+          </Link>
+        </div>
+      </ErrorMessage>
+    </div>
   );
 }
